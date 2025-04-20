f589a79474bef3e61cac4e3b0ed0133edbc294ed
Emon Hossain <emondd4@gmail.com>
2025-04-21 03:14:30 +0600

    api key init

--- lib/network/api_urls.dart
+++ lib/network/api_urls.dart
@@ -3,6 +3,8 @@ class ApiUrls {
   static final instance = ApiUrls();
 
   ///End Points
-  static String domainValidation = "/apiV2/domain-validation";
+  static String productList = "https://dummyjson.com/products?limit=100";
+  static String categoryList = "https://dummyjson.com/products/categories";
+  static String categoryProductList = "https://dummyjson.com/products/category/{category_name}";
 
 }
